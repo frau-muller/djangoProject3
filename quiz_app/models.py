ff9d0b61ae7cbc6cd121957800fdf1dbd506c82d
frau-muller <frau-muller@tut.by>
2021-11-07 20:55:14 +0300

    templ

--- quiz_app/models.py
+++ quiz_app/models.py
@@ -1,3 +1,12 @@
+import bcrypt
 from django.db import models
+from login_app.models import User  
+from django_random_queryset import RandomManager
+from questions_app.models import Category, Question 
 
-# Create your models here.
+class Score(models.Model):
+    score = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True) 
+    updated_at = models.DateTimeField(auto_now=True)
+    quiz_category = models.ForeignKey(Category, related_name='questions_in_category', on_delete=models.CASCADE) 
+    quiz_taken_by = models.ForeignKey(User, related_name='quiz_details', on_delete=models.CASCADE)
